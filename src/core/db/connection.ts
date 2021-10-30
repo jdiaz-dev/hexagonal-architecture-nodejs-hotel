e8d44c4b86f21d3c1ce56fafc85b8d969a99bdfd
Jonathan Alejandro <lordgelsin26@gmail.com>
2021-10-30 17:06:49 -0500

    seeing env variables

--- src/core/db/connection.ts
+++ src/core/db/connection.ts
@@ -2,6 +2,11 @@ import { Sequelize } from 'sequelize';
 import { SETTINGS } from '../../shared/settings/settings';
 
 const dialect = process.env.NODE_ENVIRONMENT == 'production' ? 'mariadb' : 'mysql';
+console.log('---------------env', SETTINGS.database.databaseName);
+console.log('---------------env', SETTINGS.database.usernameDB);
+console.log('---------------env', SETTINGS.database.password);
+console.log('---------------env', SETTINGS.database.host);
+
 export const db = new Sequelize(
     SETTINGS.database.databaseName,
     SETTINGS.database.usernameDB,
