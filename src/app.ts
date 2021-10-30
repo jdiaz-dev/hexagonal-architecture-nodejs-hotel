b1f7db7b7f5f0c6821ab434e80740ca15e477373
Jonathan Alejandro <lordgelsin26@gmail.com>
2021-10-30 17:09:16 -0500

    seeing NODE_ENVIRONMENT

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,5 @@ import 'reflect-metadata';
 
 import Server from './core/server';
 
-console.log('---------------------environment', process.env.PORT);
-
 const server = new Server();
 server.runServer();
--- src/core/db/connection.ts
+++ src/core/db/connection.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize';
 import { SETTINGS } from '../../shared/settings/settings';
 
 const dialect = process.env.NODE_ENVIRONMENT == 'production' ? 'mariadb' : 'mysql';
+console.log('---------------env', process.env.NODE_ENVIRONMENT);
 console.log('---------------env', SETTINGS.database.databaseName);
 console.log('---------------env', SETTINGS.database.usernameDB);
 console.log('---------------env', SETTINGS.database.password);
