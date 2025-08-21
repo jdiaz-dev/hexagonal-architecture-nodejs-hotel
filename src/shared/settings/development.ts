49abf402bc40e72e1744f01686d9db183160f8f8
polsito <lordgelsin26@gmail.com>
2025-08-21 16:53:28 -0500

    fix

--- src/shared/settings/development.ts
+++ src/shared/settings/development.ts
@@ -4,10 +4,10 @@ import { IEnvironment } from './settings.interfaces';
 export const development: IEnvironment = {
     base,
     database: {
-        databaseName: 'jdiazdevhotels',
-        usernameDB: 'root',
-        password: '',
-        host: 'localhost',
-        dialect: 'mysql',
+        databaseName: process.env.NAME as string,
+        usernameDB: process.env.USERNAME_DB as string,
+        password: process.env.PASSWORD as string,
+        host: process.env.HOST as string,
+        dialect: process.env.DIALECT as string,
     },
 };
